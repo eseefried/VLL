c854396f4c377f94dd448450f574618dc30028a5
Ethan Seefried <eseefrie@colostate.edu>
2023-12-07 10:38:52 -0700

    plz

---
+++ VLLProject/Assets/Scripts/seasonCollider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//put this script on a gameObject
+//set newObject in inspector
+//update desired tag
+public class seasonCollider : MonoBehaviour
+{
+    // This function is called when gameObject collides with another collider
+    public GameObject newObject;
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is with ObjectB
+        if (collision.gameObject.CompareTag("season")) // Check if this is the action object
+        {
+            Instantiate(newObject, transform.position, transform.rotation); //Add the new object
+            Destroy(gameObject); // Destroy the current object
+        }
+    }
+}
